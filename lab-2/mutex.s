c173dbd01006bae79f21992419b3ca97c4030a30
exsky <alex19911118@gmail.com>
2014-08-06 23:50:30 +0800

    Lab-2 DEMO

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,6 +9,16 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
+	ldr r1, =locked		@Load the locked to register1
+
+     .LOOP:			@Crital section
+	ldrex r2, [r0]		@Load register exclusive form address [r0] to r2
+	cmp r2, #0		@if register1 != unlocked
+	bne .LOOP		@Stay in crital section
+
+	strex r2, r1, [r0]	@Try to write the value of r1, to the address of r0, r2 will be 0 if succeed
+	cmp r2, #0
+	bne .LOOP		@If failes, then loop
 
         @ END CODE INSERT
 	bx lr
@@ -19,7 +29,9 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+	ldr r1, =unlocked	@Let the register1 be unlocked
+	str r1, [r0]		@Let the unlocked value store back
+
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
